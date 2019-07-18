8d15b19ce66d2453a8ca444c624467fac630c936
@Pawan.Kanvi <pawan.kanvi@lfg.com>
2019-07-17 23:27:07 -0400

    first commit

---
+++ HelloWorldTest.java
@@ -0,0 +1,10 @@
+package com.practice.jenkins;
+
+public class HelloWorldTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Jenkins Practice with Welcome to Hello world Program");
+		System.out.println("Hello World");
+	}
+
+}
